63cdd3703b6529fb097ed44cf971749032554547
Kadu <kadu.silva2014@gmail.com>
2024-09-27 15:36:23 -0300

    Feat: configura tema dark
    - usa o shadcn/ui para configurar o tema escuro
    - cria botão para alternar entrar tema claro e escuro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
@@ -15,9 +16,18 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-BR">
-      <body className={`${inter.variable} min-h-screen w-screen antialiased`}>
-        {children}
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body
+        className={`${inter.variable} min-h-screen w-screen font-sans antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
