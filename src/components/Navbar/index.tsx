bc95f584d636947b081a57a2fddaa4202ee6d283
Kadu <kadu.silva2014@gmail.com>
2024-10-04 17:45:10 -0300

    ref: reformula componente Navbar
    - implementa classe 'data-active' para estilo dinâmico de links
    - novos ícones e lógica de navegação

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,21 +1,68 @@
-import { Settings, ShoppingCart, Tags } from 'lucide-react'
+'use client'
+
+import {
+  BarChartIcon,
+  MenuIcon,
+  ShoppingCartIcon,
+  TagIcon,
+  UserIcon,
+} from 'lucide-react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export function Navbar() {
+  const pathName = usePathname()
+
+  const isProducts = pathName === '/products'
+  const isSales = pathName === '/sales'
+  const isClients = pathName === '/clients'
+  const isReport = pathName === '/report'
+  const isMenu = pathName === '/menu'
+
   return (
-    <nav className="fixed bottom-0 flex h-fit w-full items-center justify-around bg-zinc-200 p-2 dark:bg-zinc-900">
-      <Link href="/products" className="flex items-center gap-2">
-        <Tags />
-        Produtos
-      </Link>
-      <Link href="/sales" className="flex items-center gap-2">
-        <ShoppingCart />
-        Vendas
-      </Link>
-      <Link href="/settings" className="flex items-center gap-2">
-        <Settings />
-        menu
-      </Link>
-    </nav>
+    <footer className="fixed right-0 bottom-0 left-0 bg-card p-4">
+      <nav className="flex justify-around">
+        <Link
+          data-active={isProducts}
+          href="/products"
+          className="flex flex-col items-center data-[active=false]:text-secondary data-[active=true]:text-primary"
+        >
+          <TagIcon />
+          <span className=" text-xs">Produtos</span>
+        </Link>
+        <Link
+          data-active={isSales}
+          href="/sales"
+          className="flex flex-col items-center data-[active=false]:text-secondary data-[active=true]:text-primary"
+        >
+          <ShoppingCartIcon />
+          <span className=" text-xs">Vendas</span>
+        </Link>
+        <Link
+          data-active={isClients}
+          href="/clients"
+          className="flex flex-col items-center data-[active=false]:text-secondary data-[active=true]:text-primary"
+        >
+          <UserIcon />
+          <span className=" text-xs">Clientes</span>
+        </Link>
+        <Link
+          data-active={isReport}
+          href="/"
+          className="flex flex-col items-center data-[active=false]:text-secondary data-[active=true]:text-primary"
+        >
+          <BarChartIcon />
+          <span className=" text-xs">Relatórios</span>
+        </Link>
+        <Link
+          data-active={isMenu}
+          href="/"
+          className="flex flex-col items-center data-[active=false]:text-secondary data-[active=true]:text-primary"
+        >
+          <MenuIcon />
+          <span className=" text-xs">Menu</span>
+        </Link>
+      </nav>
+    </footer>
   )
 }
